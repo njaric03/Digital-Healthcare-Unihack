fdb8bb0de4a8b16e01ecec9a90733953033275d8
Nikola <n.jaric03@gmail.com>
2023-11-17 17:06:41 +0200

    Initialized views, serializers and models for doctor app, serializers needs modification

--- digital_health_care/doctor_app/views.py
+++ digital_health_care/doctor_app/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from doctor_app.models import *
+from doctor_app.serializers import *
+from django.http import HttpResponse
 
 # Create your views here.
+class SpecializationViewSet(viewsets.ModelViewSet):
+    queryset = Specialization.objects.all()
+    serializer_class = SpecializationSerializer
+
+
+class DoctorViewSet(viewsets.ModelViewSet):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
+
+class PatientViewSet(viewsets.ModelViewSet):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+
+class MedicationViewSet(viewsets.ModelViewSet):
+    queryset = Medication.objects.all()
+    serializer_class = MedicationSerializer
+
+class MedForSpecViewSet(viewsets.ModelViewSet):
+    queryset = MedForSpec.objects.all()
+    serializer_class = MedForSpecSerializer
+
+class DocMedPermissionViewSet(viewsets.ModelViewSet):
+    queryset = DocMedPermission.objects.all()
+    serializer_class = DocMedPermissionSerializer
+
+class ReceiptViewSet(viewsets.ModelViewSet):
+    queryset = Receipt.objects.all()
+    serializer_class = ReceiptSerializer
+
+class ReceiptMedicationViewSet(viewsets.ModelViewSet):
+    queryset = ReceiptMedication.objects.all()
+    serializer_class = ReceiptMedicationSerializer
+
+class PharmacyViewSet(viewsets.ModelViewSet):
+    queryset = Pharmacy.objects.all()
+    serializer_class = PharmacySerializer
+
+class HasMedicationViewSet(viewsets.ModelViewSet):
+    queryset = HasMedication.objects.all()
+    serializer_class = HasMedicationSerializer
