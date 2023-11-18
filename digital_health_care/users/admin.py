48ffbfbc92b4bf7b2f4cae910e306a385852f99c
asdasde <erceg.luka@jjzmaj.edu.rs>
2023-11-18 19:00:45 +0100

    Removed gitignore files from being tracked

--- digital_health_care/users/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . models import (Doctors, Patients, Profile)
-# Register your models here.
-
-admin.site.register(Doctors)
-admin.site.register(Patients)
-admin.site.register(Profile)
